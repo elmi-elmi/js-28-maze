675d6bc93ea30a4d05a9758d03bdb9401957aa0d
elmi <shahrokhelmi@gmail.com>
2021-08-08 19:10:13 +0430

    user win

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 10
+const cells = 3
 const width = 600
 const height = 600
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -169,6 +170,7 @@ const goal = Bodies.rectangle(
     unitLength * 0.7,
     unitLength * 0.7,
     {
+        label: 'goal',
         isStatic: true
     }
 );
@@ -178,7 +180,11 @@ World.add(world, goal)
 const ball = Bodies.circle(
     unitLength / 2,
     unitLength / 2,
-    unitLength / 3
+    unitLength / 4,
+    {
+        label: 'ball'
+
+    }
 )
 
 World.add(world, ball)
@@ -202,7 +208,16 @@ document.addEventListener('keydown', event => {
     }
     if (event.keyCode === 65) {
         Body.setVelocity(ball, { x: x - 5, y })
-
-
     }
+})
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach((collision) => {
+        const labels = ['ball', 'goal']
+
+        if (labels.includes(collision.bodyA.label)
+            && labels.includes(collision.bodyB.label)) {
+            console.log('user won!')
+        }
+    })
 })
